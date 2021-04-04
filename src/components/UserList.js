c455dba7b59599baff4e3b58aae814350ba7bae1
mavro <mrojasb2000@gmail.com>
2021-04-04 15:32:27 -0400

    Added avatar style and onclick event

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -9,7 +9,7 @@ const UserList = () => {
 
     useEffect(() => {
         getUsers();
-        console.log(users);
+        //console.log(users);
     })
 
     return (
@@ -17,8 +17,8 @@ const UserList = () => {
             <div className="list-group h-100">
                 {
                     users.map(user => (
-                        <a className="list-group-item list-group-item-action" href="#!" key={user.id}>
-                            <img src={user.avatar} className="img-fluid mr-4 rounded-circle width=70" alt="avatar"/>
+                        <a className="list-group-item list-group-item-action d-flex flex-row justify-content-start" href="#!" key={user.id} onClick={() => console.log(user.id)}>
+                            <img src={user.avatar} className="img-fluid mr-4 rounded-circle width=30" alt="avatar"/>
                             <p>
                                 {user.first_name} {user.last_name}
                             </p>
