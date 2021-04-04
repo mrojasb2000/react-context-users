14e151009a2093a6bc8b848a7c32305f1e75c0ac
mavro <mrojasb2000@gmail.com>
2021-04-04 11:39:24 -0400

    Added Profile and UserList components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
 
+import Profile from './components/Profile'
+import UserList from './components/UserList'
+
 function App() {
   return (
+    <>
+    <Profile />
+    <UserList />
     <h1>Hello World</h1>
+    </>
   );
 }
 
