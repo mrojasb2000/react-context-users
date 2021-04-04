a7f95aeef1b08e7b5b39fd28e81701d3678b3108
mavro <mrojasb2000@gmail.com>
2021-04-04 15:35:41 -0400

    Refactor on click event callback get profile by id

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -5,7 +5,7 @@ import UserContext from "../contexts/user/UserContext";
 
 const UserList = () => {
 
-    const { users, getUsers } = useContext(UserContext)
+    const { users, getUsers, getProfile } = useContext(UserContext)
 
     useEffect(() => {
         getUsers();
@@ -17,7 +17,7 @@ const UserList = () => {
             <div className="list-group h-100">
                 {
                     users.map(user => (
-                        <a className="list-group-item list-group-item-action d-flex flex-row justify-content-start" href="#!" key={user.id} onClick={() => console.log(user.id)}>
+                        <a className="list-group-item list-group-item-action d-flex flex-row justify-content-start" href="#!" key={user.id} onClick={() => getProfile(user.id)}>
                             <img src={user.avatar} className="img-fluid mr-4 rounded-circle width=30" alt="avatar"/>
                             <p>
                                 {user.first_name} {user.last_name}
